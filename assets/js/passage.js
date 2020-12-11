ba77d7b403244f9ed6157b3f00a410be1638a2f1
Aurelie <aurelie.fauquet@gmail.com>
2020-12-11 10:46:08 +0100

    delete empty files

---
+++ assets/js/passage.js
@@ -0,0 +1,10 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ */
+
+// any CSS you import will output into a single css file (passage.css in this case)
+import '../styles/passage.css';
+
+// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
+// import $ from 'jquery';
+
