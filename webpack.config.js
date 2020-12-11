7b1a9aa09f51df72d8395ee56a28f34b86d14d3f
AshBll <achillebellay@gmail.com>
2020-12-11 10:01:47 +0100

    change from scss file to css

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/js/app.js')
+    .addEntry('home', './assets/js/home.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
