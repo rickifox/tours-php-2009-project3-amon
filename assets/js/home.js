f51b3ec38c87ccdcd1ee2f5dcb3167cc4649918d
Kevin Roig <kevinroig37@gmail.com>
2020-12-16 19:48:30 +0100

    made some magic on the navbar

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,15 +1,12 @@
 import '../styles/home.css';
 
-const button1 = document.querySelector('#button-down');
-const button2 = document.querySelector('#button-up');
+var slideSource = document.getElementById('header');
 
-function show() {
-    document.getElementById('header').style.display = 'block';
+document.getElementById('button-down').onclick = function () {
+  slideSource.classList.toggle('fade');
+  diminuer();
 }
 
-function hide() {
-    document.getElementById('header').style.display = 'none';
-}
-
-button1.addEventListener('click', show);
-button2.addEventListener('click', hide);
+document.getElementById('button-up').onclick = function () {
+    slideSource.classList.toggle('fade');
+  }
