a38c2772f4f8ab48ba741199a63a32099d7510c2
Kevin Roig <kevinroig37@gmail.com>
2020-12-16 15:38:29 +0100

    modifications after merge

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,1 +1,14 @@
 import '../styles/home.css';
+let button1 = document.querySelector('#button-down');
+let button2 = document.querySelector('#button-up');
+
+button1.addEventListener("click", show);
+button2.addEventListener("click", hide);
+
+function show() {
+    document.getElementById("header").style.display = "block";
+}
+
+function hide() {
+    document.getElementById("header").style.display = "none";
+}
