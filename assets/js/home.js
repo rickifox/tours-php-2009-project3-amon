e58bd402de05bcce561694231e4d6cdcf227ff87
Kevin Roig <kevinroig37@gmail.com>
2020-12-16 15:59:48 +0100

    corrections after merging

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,14 +1,15 @@
 import '../styles/home.css';
-let button1 = document.querySelector('#button-down');
-let button2 = document.querySelector('#button-up');
 
-button1.addEventListener("click", show);
-button2.addEventListener("click", hide);
+const button1 = document.querySelector('#button-down');
+const button2 = document.querySelector('#button-up');
 
 function show() {
-    document.getElementById("header").style.display = "block";
+    document.getElementById('header').style.display = 'block';
 }
 
 function hide() {
-    document.getElementById("header").style.display = "none";
+    document.getElementById('header').style.display = 'none';
 }
+
+button1.addEventListener('click', show);
+button2.addEventListener('click', hide);
