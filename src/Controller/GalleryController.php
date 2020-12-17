137a35dd2ae6ab3fcedf94973b4246cb46810479
Kevin Roig <kevinroig37@gmail.com>
2020-12-17 15:09:55 +0100

    micro corrections

--- src/Controller/GalleryController.php
+++ src/Controller/GalleryController.php
@@ -13,22 +13,19 @@ class GalleryController extends AbstractController
      * @Route("/gallerie-design-metallique/", name="gallery_metallicDesign")
      */
     public function metallicDesignGallery(): Response
-    {       
+    {
         $images = $this->getDoctrine()
         ->getRepository(Image::class)
         ->findAll();
-        return $this->render('gallery/metallicDesign.html.twig', ['images'=> $images]);
+        return $this->render('gallery/metallicDesign.html.twig', ['images' => $images]);
     }
-
-
-
 
     /**
      * @Route("/gallerie-design-metallique/{id}", name="gallerie-design-metallique2")
      */
     public function metallicDesignGalleryDONOTUSE(Image $image): Response
-    {       
-        return $this->render('gallery/metallic_design.html.twig', ['image'=> $image]);
+    {
+        return $this->render('gallery/metallic_design.html.twig', ['image' => $image]);
     }
 
         /**
