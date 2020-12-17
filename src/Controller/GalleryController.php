b17c1df9ca305bac87c099ca3a2171e74889c518
Kevin Roig <kevinroig37@gmail.com>
2020-12-17 12:05:45 +0100

    , created database, created metallicDesign gallery

--- src/Controller/GalleryController.php
+++ src/Controller/GalleryController.php
@@ -5,15 +5,30 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Image;
 
 class GalleryController extends AbstractController
 {
     /**
-     * @Route("/gallerie-design-metallique", name="gallerie-design-metallique")
+     * @Route("/gallerie-design-metallique/", name="gallery_metallicDesign")
      */
     public function metallicDesignGallery(): Response
-    {
-        return $this->render('gallery/metallic_design.html.twig');
+    {       
+        $images = $this->getDoctrine()
+        ->getRepository(Image::class)
+        ->findAll();
+        return $this->render('gallery/metallicDesign.html.twig', ['images'=> $images]);
+    }
+
+
+
+
+    /**
+     * @Route("/gallerie-design-metallique/{id}", name="gallerie-design-metallique2")
+     */
+    public function metallicDesignGalleryDONOTUSE(Image $image): Response
+    {       
+        return $this->render('gallery/metallic_design.html.twig', ['image'=> $image]);
     }
 
         /**
---
+++ src/Entity/Image.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ImageRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ImageRepository::class)
+ */
+class Image
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $url;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $categorie;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $texte_alternatif;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Article::class, mappedBy="image")
+     */
+    private $articles;
+
+    public function __construct()
+    {
+        $this->articles = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(string $url): self
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    public function getCategorie(): ?string
+    {
+        return $this->categorie;
+    }
+
+    public function setCategorie(string $categorie): self
+    {
+        $this->categorie = $categorie;
+
+        return $this;
+    }
+
+    public function getTexteAlternatif(): ?string
+    {
+        return $this->texte_alternatif;
+    }
+
+    public function setTexteAlternatif(?string $texte_alternatif): self
+    {
+        $this->texte_alternatif = $texte_alternatif;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Article[]
+     */
+    public function getArticles(): Collection
+    {
+        return $this->articles;
+    }
+
+    public function addArticle(Article $article): self
+    {
+        if (!$this->articles->contains($article)) {
+            $this->articles[] = $article;
+            $article->addImage($this);
+        }
+
+        return $this;
+    }
+
+    public function removeArticle(Article $article): self
+    {
+        if ($this->articles->removeElement($article)) {
+            $article->removeImage($this);
+        }
+
+        return $this;
+    }
+}
