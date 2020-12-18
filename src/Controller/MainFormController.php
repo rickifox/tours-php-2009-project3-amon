6d548ed4db81498089fccd5e563c5d13ce84d554
Kevin Roig <kevinroig37@gmail.com>
2020-12-18 10:53:14 +0100

    minor changes on MainFormController

--- src/Controller/MainFormController.php
+++ src/Controller/MainFormController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Image;
 use App\Form\MainFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,13 +16,12 @@ class MainFormController extends AbstractController
     /**
      * @Route("/form/", name="form")
      */
-    public function index(Request $request): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
         $image = new Image();
         $form = $this->createForm(MainFormType::class, $image);
         $form->handleRequest($request);
-        if ($form->isSubmitted()) {
-            $entityManager = $this->getDoctrine()->getManager();
+        if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->persist($image);
             $entityManager->flush();
             return $this->redirectToRoute('form');
