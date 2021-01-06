4d6154d7a399bf79b9d50d3c61982006a070b463
Kevin Roig <kevinroig37@gmail.com>
2021-01-06 11:08:10 +0100

    some changes on form

--- src/Form/MainFormType.php
+++ src/Form/MainFormType.php
@@ -7,6 +7,9 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class MainFormType extends AbstractType
 {
@@ -14,8 +17,14 @@ class MainFormType extends AbstractType
     {
         $builder
             ->add('nom', TextType::class)
-            ->add('url', TextType::class)
-            ->add('categorie', TextType::class)
+            ->add('url', FileType::class, [
+            'label' =>'Selectionner une photo'])
+            ->add('categorie', ChoiceType::class, [
+                'choices' => [
+                    'Chats' => 'Chats',
+                    'Chiens' => 'Chiens'
+                ]
+            ])
             ->add('texte_alternatif', TextType::class)
         ;
     }
