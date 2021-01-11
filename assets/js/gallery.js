97123cbf5c1d9e98df7cc5f35836b8a17bc2a772
rickifox <rickipereira3@gmail.com>
2021-01-11 10:39:35 +0100

    fixed some minor PSR problems and turn the X into a category navbar closing-button

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -5,19 +5,22 @@
 // any CSS you import will output into a single css file (gallery.css in this case)
 import '../styles/gallery.css';
 
-/* Set the width of the side navigation to 14rem on click or toggle it back to 0(category button width) */
+/* Set the width of the side navigation to 14rem (category button width) 
+on click or toggle it back to 0 */
 function toggleSideNav() {
-    let sidenav = document.getElementById("mySidenav");
-    if (sidenav.style.width === "14rem") {
-        sidenav.style.width = "0";
+    const sidenav = document.getElementById('mySidenav');
+    if (sidenav.style.width === '14rem') {
+        sidenav.style.width = '0';
     } else {
-        sidenav.style.width ="14rem";
-    };
-};  
-document.getElementById("categories-pop").addEventListener("click", toggleSideNav, false);
-document.getElementById("").addEventListener("click", toggleSideNav, false);
+        sidenav.style.width = '14rem';
+    }
+}
+
+
 /* Event listener to trigger the toggleSideNav function*/
 
+document.getElementById('categories-pop').addEventListener('click', toggleSideNav, false);
+document.getElementById('closebtn').addEventListener('click', toggleSideNav, false);
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
