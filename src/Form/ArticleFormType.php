5337ab007a2defd83e30c5730134bac79b46366e
Kevin Roig <kevinroig37@gmail.com>
2021-01-11 10:42:50 +0100

    inor changes to fix check

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -11,7 +11,6 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
-
 class ArticleFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
