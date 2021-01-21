6a35e1925eda2d818307f8228c6eac583b01f3a9
Kevin Roig <kevinroig37@gmail.com>
2021-01-21 19:20:04 +0100

    editing an article is possible, but the images are deleted from db also, more work to do on that

--- src/Controller/ActualityController.php
+++ src/Controller/ActualityController.php
@@ -3,10 +3,15 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Entity\Image;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Entity;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\ArticleFormType;
 
 class ActualityController extends AbstractController
 {
@@ -15,11 +20,15 @@ class ActualityController extends AbstractController
      */
     public function showArticles(ArticleRepository $articleRepository): Response
     {
-        $articles = $articleRepository->findAll();
+        $articles = $articleRepository->findBy(
+            ["section" => 'Actualites'],
+            ['id' => 'DESC'],
+        );
         return $this->render('actuality/index.html.twig', ['articles' => $articles]);
     }
 
     /**
+     * Returning the 3 last articles posted, for the carousel in "Design métallique"
      * @return object[]
      */
     public function showCarouselArticles(ArticleRepository $articleRepository): array
@@ -31,5 +40,27 @@ class ActualityController extends AbstractController
             0,
         );
         return $articles;
+    }
+
+    /**
+     * Editing an article, but can not delete image for now, or it goes out of the db too.
+     * @Route("/actualites/{id}/edit", name="actuality_edit")
+     */
+    public function editArticles(Request $request, Article $article): Response
+    {
+        /*$article->getImage()->removeElement($image);
+        $image->getArticles()->removeElement($article);*/
+        $form = $this->createForm(ArticleFormType::class, $article);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+        
+            return $this->redirectToRoute('actuality');
+        }
+        
+        return $this->render('actuality/edit.html.twig', [
+            'article' => $article,
+            'form' => $form->createView(),
+        ]);
     }
 }
